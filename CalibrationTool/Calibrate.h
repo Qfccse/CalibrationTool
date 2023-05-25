d502f7362a614e977a966ebb38e92adb98bfdf2f
Sea <2534062529@qq.com>
2023-05-25 16:50:54 +0800

    ui

--- CalibrationTool/Calibrate.h
+++ CalibrationTool/Calibrate.h
@@ -2,3 +2,11 @@
 #include "Const.h"
 
 CalibrateResults calibrate(QStringList fileNames, int cameraType);
+
+double calculateReprojectionError(const vector<Point3f>& objectPoints,
+    const vector<Point2f>& imagePoints,
+    const Mat& cameraMatrix,
+    const Mat& distCoeffs,
+    const Mat& rvec,
+    const Mat& tvec,
+    const int cameraType);
