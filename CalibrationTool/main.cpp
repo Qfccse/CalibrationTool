11fff82d2709ca95273c94f70e9a368b85f7e78f
LUCKYDOGQAQ <101766153+LUCKYDOGQAQ@users.noreply.github.com>
2023-05-31 10:56:55 +0800

    add axis

--- CalibrationTool/main.cpp
+++ CalibrationTool/main.cpp
@@ -1,35 +1,17 @@
 #include "CalibrationTool.h"
 #include <QtWidgets/QApplication>
 
-// 
+
 #include <QGuiApplication>
-
-#include <Qt3DRender/qcamera.h>
-#include <Qt3DCore/qentity.h>
-#include <Qt3DRender/qcameralens.h>
-
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QCheckBox>
 #include <QtWidgets/QCommandLinkButton>
 #include <QtGui/QScreen>
 
-#include <Qt3DRender/qmesh.h>
-#include <Qt3DRender/qtechnique.h>
-#include <Qt3DRender/qmaterial.h>
-#include <Qt3DRender/qeffect.h>
-#include <Qt3DRender/qtexture.h>
-#include <Qt3DRender/qrenderpass.h>
-#include <Qt3DRender/qsceneloader.h>
-#include <Qt3DRender/qpointlight.h>
-
-#include <Qt3DCore/qtransform.h>
-#include <Qt3DCore/qaspectengine.h>
-
-#include <Qt3DRender/qrenderaspect.h>
-//#include <Qt3DExtras/qforwardrenderer.h>
+#include <Qt3DCore>
+#include <Qt3DRender>
 #include <Qt3DExtras>
-#include <QGuiApplication>
 
 int main(int argc, char *argv[])
 {
@@ -37,27 +19,31 @@ int main(int argc, char *argv[])
     CalibrationTool w;
     w.show();
 
+    // Root entity
+    Qt3DCore::QEntity* rootEntity = new Qt3DCore::QEntity();
+
     Qt3DExtras::Qt3DWindow* view = new Qt3DExtras::Qt3DWindow();
     view->defaultFrameGraph()->setClearColor(QColor(QRgb(0xffffffff)));
     QWidget* container = QWidget::createWindowContainer(view);
     QSize screenSize = view->screen()->size();
     container->setMinimumSize(QSize(200, 100));
     container->setMaximumSize(screenSize);
+    view->setRootEntity(rootEntity);
 
     QWidget* widget = new QWidget;
+    // layout
     QHBoxLayout* hLayout = new QHBoxLayout(widget);
     QVBoxLayout* vLayout = new QVBoxLayout();
     vLayout->setAlignment(Qt::AlignTop);
     hLayout->addWidget(container, 1);
     hLayout->addLayout(vLayout);
-
     widget->setWindowTitle(QStringLiteral("Basic shapes"));
-
-    // Root entity
-    Qt3DCore::QEntity* rootEntity = new Qt3DCore::QEntity();
 
     // Camera
     Qt3DRender::QCamera* cameraEntity = view->camera();
+    // For camera controls
+    Qt3DExtras::QFirstPersonCameraController* camController = new Qt3DExtras::QFirstPersonCameraController(rootEntity);
+    camController->setCamera(cameraEntity);
 
     cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f);
     cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
@@ -74,35 +60,83 @@ int main(int argc, char *argv[])
     lightEntity->addComponent(lightTransform);
 
 
-
     // Cuboid shape data
     Qt3DExtras::QCuboidMesh* cuboid = new Qt3DExtras::QCuboidMesh();
-
     // CuboidMesh Transform
     Qt3DCore::QTransform* cuboidTransform = new Qt3DCore::QTransform();
     cuboidTransform->setScale(4.0f);
-    cuboidTransform->setTranslation(QVector3D(5.0f, -4.0f, 0.0f));
-
+    cuboidTransform->setTranslation(QVector3D(0.0f, 0.0f, 0.0f));
+    // CuboidMesh Material
     Qt3DExtras::QPhongMaterial* cuboidMaterial = new Qt3DExtras::QPhongMaterial();
     cuboidMaterial->setDiffuse(QColor(QRgb(0x665423)));
-
-
     //Cuboid
-    Qt3DCore::QEntity* m_cuboidEntity;
-    m_cuboidEntity = new Qt3DCore::QEntity(rootEntity);
-    m_cuboidEntity->addComponent(cuboid);
-    m_cuboidEntity->addComponent(cuboidMaterial);
-    m_cuboidEntity->addComponent(cuboidTransform);
+    Qt3DCore::QEntity* cuboidEntity = new Qt3DCore::QEntity(rootEntity);
+    cuboidEntity->addComponent(cuboid);
+    cuboidEntity->addComponent(cuboidMaterial);
+    cuboidEntity->addComponent(cuboidTransform);
 
-    // For camera controls
-    Qt3DExtras::QFirstPersonCameraController* camController = new Qt3DExtras::QFirstPersonCameraController(rootEntity);
-    camController->setCamera(cameraEntity);
+    // Axis entity
+    Qt3DCore::QEntity* axisEntity = new Qt3DCore::QEntity(rootEntity);
 
-    view->setRootEntity(rootEntity);
+    // X-axis
+    // X-axis mesh
+    Qt3DExtras::QCylinderMesh* xAxisMesh = new Qt3DExtras::QCylinderMesh();
+    xAxisMesh->setRadius(0.05f);
+    xAxisMesh->setLength(20.0f);
+    // X-axis material
+    Qt3DExtras::QPhongMaterial* xAxisMaterial = new Qt3DExtras::QPhongMaterial();
+    xAxisMaterial->setAmbient(Qt::red); //color
+    // X-axis transform
+    Qt3DCore::QTransform* xAxisTransform = new Qt3DCore::QTransform();
+    xAxisTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(0, 0, 1), 90.0f));
+    xAxisTransform->setTranslation(QVector3D(0.0f, 0.0f, 0.0f));
+    // X-axis entity
+    Qt3DCore::QEntity* xAxisEntity = new Qt3DCore::QEntity(axisEntity);
+    xAxisEntity->addComponent(xAxisMesh);
+    xAxisEntity->addComponent(xAxisMaterial);
+    xAxisEntity->addComponent(xAxisTransform);
+
+
+    // Y-axis
+    // Y-axis mesh
+    Qt3DExtras::QCylinderMesh* yAxisMesh = new Qt3DExtras::QCylinderMesh();
+    yAxisMesh->setRadius(0.05f);
+    yAxisMesh->setLength(20.0f);
+    // Y-axis material
+    Qt3DExtras::QPhongMaterial* yAxisMaterial = new Qt3DExtras::QPhongMaterial();
+    yAxisMaterial->setAmbient(Qt::blue); //color
+    // Y-axis transform
+    Qt3DCore::QTransform* yAxisTransform = new Qt3DCore::QTransform();
+    yAxisTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), -90.0f));
+    yAxisTransform->setTranslation(QVector3D(0.0f, 0.0f, 0.0f));
+
+    Qt3DCore::QEntity* yAxisEntity = new Qt3DCore::QEntity(axisEntity);
+    yAxisEntity->addComponent(yAxisMesh);
+    yAxisEntity->addComponent(yAxisMaterial);
+    yAxisEntity->addComponent(yAxisTransform);
+
+
+    // Z-axis
+    // Z-axis mesh
+    Qt3DExtras::QCylinderMesh* zAxisMesh = new Qt3DExtras::QCylinderMesh();
+    zAxisMesh->setRadius(0.05f);
+    zAxisMesh->setLength(20.0f);
+    // Z-axis material
+    Qt3DExtras::QPhongMaterial* zXxisMaterial = new Qt3DExtras::QPhongMaterial();
+    zXxisMaterial->setAmbient(Qt::yellow); //color
+    // Z-axis transform
+    Qt3DCore::QTransform* zAxisTransform = new Qt3DCore::QTransform();
+    //zAxisTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(0, 0, 1), 90.0f));
+    zAxisTransform->setTranslation(QVector3D(0.0f, 0.0f, 0.0f));
+
+    Qt3DCore::QEntity* zAxisEntity = new Qt3DCore::QEntity(axisEntity);
+    zAxisEntity->addComponent(zAxisMesh);
+    zAxisEntity->addComponent(zXxisMaterial);
+    zAxisEntity->addComponent(zAxisTransform);
 
     // Show window
     widget->show();
-    widget->resize(1200, 800);
+    widget->resize(500, 500);
 
     return a.exec();
 }
